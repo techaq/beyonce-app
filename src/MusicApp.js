2d475af78c307e380199e1bf13dfce16a7b98980
Fatimah <aishafhonore@gmail.com>
2023-08-25 00:08:54 -0500

    8/25

--- src/MusicApp.js
+++ src/MusicApp.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function MusicApp() {
+  // State variables to manage user input and fetched data
   const [artistName, setArtistName] = useState("");
   const [artistOptions, setArtistOptions] = useState([]);
   const [trackData, setTrackData] = useState([]);
   const [searching, setSearching] = useState(false);
 
+  // Function to handle the search button click
   const handleSearchClick = () => {
     if (artistName.trim() !== "") {
-      setSearching(true);
+      setSearching(true); // Initiate search when the button is clicked
     }
   };
 
+  // Function to fetch top tracks for the selected artist
   const fetchTopTracks = (artistName) => {
     fetch(
       `http://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=${artistName}&api_key=5c4af263893f04cb0df26a7aebf93dcb&format=json`
     )
       .then((response) => response.json())
       .then((data) => {
-        setTrackData(data.toptracks.track);
+        setTrackData(data.toptracks.track); // Set fetched top tracks data
       })
       .catch((error) => console.error("Error fetching top tracks:", error));
   };
 
+  // Effect to fetch artist options based on user input
   useEffect(() => {
     if (searching) {
       fetch(
@@ -35,14 +39,14 @@ function MusicApp() {
             data.results.artistmatches &&
             data.results.artistmatches.artist
           ) {
-            setArtistOptions(data.results.artistmatches.artist);
+            setArtistOptions(data.results.artistmatches.artist); // Set fetched artist options
           } else {
-            setArtistOptions([]);
+            setArtistOptions([]); // Clear options if no matching artists found
           }
         })
         .catch((error) => console.error("Error fetching data:", error))
         .finally(() => {
-          setSearching(false);
+          setSearching(false); // Reset searching state
         });
     }
   }, [searching, artistName]);
@@ -63,13 +67,14 @@ function MusicApp() {
           />
           {artistOptions.length > 0 && !searching && (
             <ul className="artist-options">
+              {/* Render artist options as list items */}
               {artistOptions.map((artist, index) => (
                 <li
                   key={index}
                   onClick={() => {
                     setArtistName(artist.name);
-                    setArtistOptions([]);
-                    fetchTopTracks(artist.name);
+                    setArtistOptions([]); // Clear options after artist selection
+                    fetchTopTracks(artist.name); // Fetch top tracks for selected artist
                   }}
                 >
                   {artist.name}
@@ -84,6 +89,7 @@ function MusicApp() {
         <div>
           <h2>Top Tracks for {artistName}</h2>
           <ul className="track-list">
+            {/* Render top tracks as list items */}
             {trackData.map((track, index) => (
               <li key={index}>{track.name}</li>
             ))}
